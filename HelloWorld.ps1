c3c1c9d19a9c428cb1424155966575ca4aada20e
ragu <ragusenthilrajan@gmail.com>
2018-05-10 15:11:45 +0530

    updated the ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,1 +1,1 @@
-Write-Host "Hello world"
+contentful space migration --space-id '6wcn6sijwr5o' --environment-id 'dev' --access-token 'ea88e96b071ce610b5f4f9cd39a318204f71cab2ffd8dff2065e0a9db32f5717' add_content_field.js
