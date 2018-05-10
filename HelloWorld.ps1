e77925a34ee54153adf343fbf02490bb882232b3
ragu <ragusenthilrajan@gmail.com>
2018-05-10 15:19:23 +0530

    updated

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,1 +1,3 @@
+npm install contentful-cli -g
+
 contentful space migration --space-id '6wcn6sijwr5o' --environment-id 'dev' --access-token 'ea88e96b071ce610b5f4f9cd39a318204f71cab2ffd8dff2065e0a9db32f5717' add_content_field.js
